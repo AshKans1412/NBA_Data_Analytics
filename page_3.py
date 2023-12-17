d5a4fcf71e6caff89cacb7b0e81d4cc37cfed133
Ash Kans <71004480+AshKans1412@users.noreply.github.com>
2023-12-17 12:25:13 -0700

    Update page_3.py

--- page_3.py
+++ page_3.py
@@ -229,6 +229,11 @@ def live_page(source='local'):
                     st.text("")
                     st.text("")
                     st.text("")
+
+                    st.text("")
+                    st.text("")
+                    st.text("")
+                    st.text("")
                     
                     # Draw line charts
                     home_team_scores = get_period_scores(home_team)
