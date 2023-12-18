a64ef2b552d893ca2efa792afdd218c24be36ab2
Ash Kans <71004480+AshKans1412@users.noreply.github.com>
2023-12-18 02:52:33 -0700

    Update page_3.py

--- page_3.py
+++ page_3.py
@@ -303,8 +303,8 @@ def live_page(source='local'):
             tri_home = home_team["teamTricode"]
             away_home = away_team["teamTricode"]
 
-            request_pred = f"https://ash-nba-api-ea2ef5de0ea1.herokuapp.com/predict?team1={tri_home}&team2={away_home}"
-            #request_pred_1 = requests.get(request_pred).json()
+            request_pred = f"https://nba-api-ash-1-fc1674476d71.herokuapp.com/predict?team1={tri_home}&team2={away_home}"
+            request_pred_1 = requests.get(request_pred).json()
 
             st.write(f"Predicted Winner: {request_pred}")
             st.markdown("---")  # Separator line
