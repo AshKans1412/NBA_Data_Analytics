e931312d4bc5a279c75ae591977defcd82acdb6d
Ash Kans <71004480+AshKans1412@users.noreply.github.com>
2024-01-25 23:34:21 -0700

    Update page2.py

--- page2.py
+++ page2.py
@@ -7,7 +7,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
-import json
+from flask import Flask, jsonify, request
 
 # Function to find the closest match using difflib
 def find_closest_match(user_input, candidate_list):
